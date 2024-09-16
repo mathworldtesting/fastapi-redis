a29fe2efa13c42b699817d006d7120ced573bac2
mathworldtesting <mathworldtesting@outlook.com>
2024-09-15 23:01:34 -0500

    updated

--- main.py
+++ main.py
@@ -61,5 +61,5 @@ def fetch_football_details(league: str, season: str):
 
 
 
-if __name__ == "__main__":
-    uvicorn.run("main:app", port=8000, reload=True)
+# if __name__ == "__main__":
+#     uvicorn.run("main:app", port=8000, reload=True)
